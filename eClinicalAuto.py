6e71f9e8f43cf5f38f7c03ffbc73a90c6c9033a3
Justin Mercado <justinreianmercado@gmail.com>
2020-07-08 22:02:57 -0700

    Version 3.0
    Added support for SSN, Capitilizing first and last name, and secondary insurance. Also automatically ends on address verification after all info is inputted.

--- eClinicalAuto.py
+++ eClinicalAuto.py
@@ -1,6 +1,7 @@
 from pyautogui import *
 from tkinter import *
 from time import sleep
+from pyperclip import paste
 
 # Create Window
 root = Tk()
@@ -33,6 +34,12 @@ def enterPatientInfo():
     label_primaryInsuranceName.grid(row=3, column=0)
     label_primarySubscriberNum = Label(root_enterPatientInfo, text="Primary Insurance Number")
     label_primarySubscriberNum.grid(row=4, column=0)
+    label_secondaryInsuranceName = Label(root_enterPatientInfo, text="Secondary Insurance Name")
+    label_secondaryInsuranceName.grid(row=5, column=0)
+    label_secondarySubscriberNum = Label(root_enterPatientInfo, text="Secondary Insurance Number")
+    label_secondarySubscriberNum.grid(row=6, column=0)
+    label_ssn = Label(root_enterPatientInfo, text="SSN")
+    label_ssn.grid(row=7, column=0)
 
     # Create Entries
     entry_address = Entry(root_enterPatientInfo)
@@ -45,25 +52,38 @@ def enterPatientInfo():
     entry_primaryInsuranceName.grid(row=3, column=1)
     entry_primarySubscriberNum = Entry(root_enterPatientInfo)
     entry_primarySubscriberNum.grid(row=4, column=1)
+    entry_secondaryInsuranceName = Entry(root_enterPatientInfo)
+    entry_secondaryInsuranceName.grid(row=5, column=1)
+    entry_secondarySubscriberNum = Entry(root_enterPatientInfo)
+    entry_secondarySubscriberNum.grid(row=6, column=1)
+    entry_ssn = Entry(root_enterPatientInfo)
+    entry_ssn.grid(row=7, column=1)
+
+    # Create Capitalize Name Check Button and Variable
+    capsNameState = IntVar()
+    check_capitalizeName = Checkbutton(root_enterPatientInfo, text="Caps name?", variable=capsNameState)
+    check_capitalizeName.grid(row=8, column=0, columnspan=2)
 
     # Create Radio Buttons
+    label_facilitySelect = Label(root_enterPatientInfo, text="Default Facility")
+    label_facilitySelect.grid(row=9, column=0, columnspan=2)
     radio_facilitySelect =  IntVar()
     radio_Aspen = Radiobutton(root_enterPatientInfo, text="Aspen", variable=radio_facilitySelect, value=1)
-    radio_Aspen.grid(row=5, column=0)
+    radio_Aspen.grid(row=10, column=0)
     radio_Princeton = Radiobutton(root_enterPatientInfo, text="Princeton", variable=radio_facilitySelect, value=2)
-    radio_Princeton.grid(row=5, column=1)
+    radio_Princeton.grid(row=10, column=1)
     radio_Mcclure = Radiobutton(root_enterPatientInfo, text="Mcclure", variable=radio_facilitySelect, value=3)
-    radio_Mcclure.grid(row=6, column=0)
+    radio_Mcclure.grid(row=11, column=0)
     radio_Moraga = Radiobutton(root_enterPatientInfo, text="Moraga", variable=radio_facilitySelect, value=4)
-    radio_Moraga.grid(row=6, column=1)
+    radio_Moraga.grid(row=11, column=1)
     radio_Marin = Radiobutton(root_enterPatientInfo, text="Marin", variable=radio_facilitySelect, value=5)
-    radio_Marin.grid(row=7, column=0)
+    radio_Marin.grid(row=12, column=0)
     radio_Legacy = Radiobutton(root_enterPatientInfo, text="Legacy", variable=radio_facilitySelect, value=6)
-    radio_Legacy.grid(row=7, column=1)
+    radio_Legacy.grid(row=12, column=1)
     radio_Oakland = Radiobutton(root_enterPatientInfo, text="Oakland", variable=radio_facilitySelect, value=7)
-    radio_Oakland.grid(row=8, column=0)
+    radio_Oakland.grid(row=13, column=0)
     radio_Other = Radiobutton(root_enterPatientInfo, text="Other", variable=radio_facilitySelect, value=8)
-    radio_Other.grid(row=8, column=1)
+    radio_Other.grid(row=13, column=1)
 
     # Create Variables 
     address = StringVar()
@@ -71,7 +91,11 @@ def enterPatientInfo():
     phone = StringVar()
     primaryInsuranceName = StringVar()
     primarySubscriberNum = StringVar()
+    secondaryInsuranceName = StringVar()
+    secondarySubscriberNum = StringVar()
+    ssn = StringVar()
     facilityName = StringVar()
+
 
     # Get information from entries and assign to variables
     def getInfoFromEntries():
@@ -100,6 +124,21 @@ def enterPatientInfo():
             primarySubscriberNum.set("000000")
         else:
             primarySubscriberNum.set(entry_primarySubscriberNum.get())
+        # Secondary Insurance Name
+        if entry_secondaryInsuranceName.get() == "":
+            secondaryInsuranceName.set("NONE")
+        else:
+            secondaryInsuranceName.set(entry_secondaryInsuranceName.get())
+        # Secondary Insurance Number
+        if entry_secondarySubscriberNum.get() == "":
+            secondarySubscriberNum.set("000000")
+        else:
+            secondarySubscriberNum.set(entry_secondarySubscriberNum.get())
+        # SSN
+        if entry_ssn.get() == "":
+            ssn.set("")
+        else:
+            ssn.set(entry_ssn.get())
         # Facility
         if radio_facilitySelect.get() == 1:
             facilityName.set("Aspen")
@@ -117,6 +156,8 @@ def enterPatientInfo():
             facilityName.set("Oakland")
         else:
             facilityName.set("SkilledMD")
+        
+        print(capsNameState.get())
 
     # Begin to autofill fields
     def beginAutoPatientInfo():
@@ -127,6 +168,26 @@ def enterPatientInfo():
         moveTo(820, 291, duration=0)
         click()
         time.sleep(2) # Delay for patient info to load
+
+         # Capitalize name
+        if capsNameState.get() == 1:
+            # Cut Last Name, Caps it, Then Paste
+            moveTo(843, 269)
+            dragTo(637, 270, duration=2)
+            keyDown("ctrl")
+            press("x")
+            keyUp("ctrl")
+            lastName = str(paste()).upper()
+            write(lastName)
+
+            # Cut First Name, Caps it, Then Paste
+            moveTo(891, 293)
+            dragTo(702, 294, duration=2)
+            keyDown("ctrl")
+            press("x")
+            keyUp("ctrl")
+            firstName = str(paste()).upper()
+            write(firstName)
 
         # Enter address info
         moveTo(813, 333, duration=0)
@@ -157,6 +218,11 @@ def enterPatientInfo():
         moveTo(1265, 269, duration=0)
         click()
 
+        # Enter SSN info
+        moveTo(1082, 415, duration=0)
+        click()
+        write(ssn.get())
+
         # Click on Add Insurance Button
         moveTo(1124, 658, duration=0)
         click()
@@ -181,6 +247,31 @@ def enterPatientInfo():
         moveTo(1263, 790, duration=0)
         click()
 
+        if secondaryInsuranceName.get() != "NONE" and secondarySubscriberNum.get() != "000000":
+            # Click on Add Insurance Button
+            moveTo(1124, 658, duration=0)
+            click()
+            time.sleep(1) # Delay to let insurance field load
+            # Search for secondary insurance
+            write(secondaryInsuranceName.get().upper())
+            time.sleep(.5) # Delay to let filtered insurance load
+            # Select top result found
+            moveTo(1116, 438, duration=0)
+            click()
+            moveTo(1274, 763, duration=0)
+            click()
+            time.sleep(.5) # Delay to let filtered insurance load
+            # Check Secondary Insurance Checkbox
+            moveTo(857, 330, duration=0)
+            click()
+            # Enter Secondary Subscriber Number
+            moveTo(693, 472, duration=0)
+            click()
+            write(secondarySubscriberNum.get().upper())
+            # Save Secondary Insurance Info
+            moveTo(1263, 790, duration=0)
+            click()
+
         # Click Additional Info
         moveTo(679, 867, duration=0)
         click()
@@ -200,10 +291,16 @@ def enterPatientInfo():
         # Save Additional Info Changes
         moveTo(1206, 881, duration=0)
         click()
+        sleep(1.5) # Delay to let main menu load
+
+        # Validate Address and allow for human review
+        moveTo(916, 384, duration=0)
+        click()
 
     # Create Submit Button
     button_submitPatientInfo = Button(root_enterPatientInfo, text="Submit", padx=5, pady=5, command=beginAutoPatientInfo)
-    button_submitPatientInfo.grid(row=9, column=0, columnspan=2)
+    button_submitPatientInfo.grid(row=14, column=0, columnspan=2)
+    
 
 # Attempts to post all ERA's on a given range
 def postERA():
